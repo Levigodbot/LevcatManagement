e0af0f9a290ebed8117729bd6f7271a375046718
AzmiAbdillah <95601238+Levigodbot@users.noreply.github.com>
2022-07-31 13:37:42 +0700

    Update antiflood.py

--- RocksAlexaRobot/modules/antiflood.py
+++ RocksAlexaRobot/modules/antiflood.py
@@ -361,7 +361,7 @@ will result in restricting that user.
  `1w` = 1 week
  """
 
-__mod_name__ = "🔕 ғʟᴏᴏᴅ"
+__mod_name__ = "ғʟᴏᴏᴅ"
 
 FLOOD_BAN_HANDLER = MessageHandler(
     Filters.all & ~Filters.status_update & Filters.group, check_flood)
