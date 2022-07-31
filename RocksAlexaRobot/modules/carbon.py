df31f8410dc4716ca851059c054f9c82e79691ed
AzmiAbdillah <95601238+Levigodbot@users.noreply.github.com>
2022-07-31 13:41:22 +0700

    Update carbon.py

--- RocksAlexaRobot/modules/carbon.py
+++ RocksAlexaRobot/modules/carbon.py
@@ -8,13 +8,13 @@ from RocksAlexaRobot.utils.make_carbon import make_carbon
 @capture_err
 async def carbon_func(_, message):
     if not message.reply_to_message:
-        return await message.reply_text("**🙄Reply to a text message to make carbon.**")
+        return await message.reply_text("**Reply to a text message to make carbon.**")
     if not message.reply_to_message.text:
-        return await message.reply_text("**🙄Reply to a text message to make carbon.**")
+        return await message.reply_text("**Reply to a text message to make carbon.**")
     m = await message.reply_text("**⬇Downloading...**")
     carbon = await make_carbon(message.reply_to_message.text)
     await m.edit("**⬆Uploading...**")
-    msg = "**Made by @Alexa_Help**"
+    msg = "**Made by Levcat**"
     await pbot.send_photo(message.chat.id, carbon,caption=msg)
     await m.delete()
     carbon.close()
