5b80876242a9538c9e59de19bc43fef8a13c067a
AzmiAbdillah <95601238+Levigodbot@users.noreply.github.com>
2022-07-31 14:36:30 +0700

    Update speed_test.py

--- RocksAlexaRobot/modules/speed_test.py
+++ RocksAlexaRobot/modules/speed_test.py
@@ -66,6 +66,6 @@ SPEED_TEST_CALLBACKHANDLER = CallbackQueryHandler(
 dispatcher.add_handler(SPEED_TEST_HANDLER)
 dispatcher.add_handler(SPEED_TEST_CALLBACKHANDLER)
 
-__mod_name__ = "🚅 SᴘᴇᴇᴅTᴇsᴛ"
+__mod_name__ = "SᴘᴇᴇᴅTᴇsᴛ"
 __command_list__ = ["speedtest"]
 __handlers__ = [SPEED_TEST_HANDLER, SPEED_TEST_CALLBACKHANDLER]
